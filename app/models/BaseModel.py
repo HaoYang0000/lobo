b81be714180b836474d4600be770d56363c18609
Hao Yang <hao.yang@Hao-Yangs-MacBook-Pro.local>
2018-10-12 23:09:05 -0500

    Build base, user model

---
+++ app/models/BaseModel.py
@@ -0,0 +1,48 @@
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import ModelSchema
+from sqlalchemy import Column, text
+from sqlalchemy.dialects import mysql
+
+db = SQLAlchemy()
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    __table_args__ = (
+        {
+            'mysql_engine': 'InnoDB',
+            'mysql_charset': 'utf8mb4',
+            'mysql_collate': 'utf8mb4_unicode_ci'
+        }
+    )
+
+class BaseModelExtended(BaseModel):
+    __abstract__ = True
+
+    id = Column(
+        mysql.INTEGER(unsigned=True, display_width=11),
+        primary_key=True,
+        autoincrement=True,
+        nullable=False
+    )
+    created_at = Column(
+        mysql.TIMESTAMP,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP")
+    )
+    updated_at = Column(
+        mysql.TIMESTAMP,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    )
+
+class BaseSchema(ModelSchema):
+    def __init__(self, wrap=True, *args, **kwargs):
+        self.wrap = wrap
+        super().__init__(*args, **kwargs)
+
+class BaseMeta:
+    include_fk = True
+    dateformat = '%Y-%m-%d %H:%M:%S'
+    sqla_session = db.session
+    strict = True
---
+++ app/models/UserModel.py
@@ -0,0 +1,57 @@
+from app.models.BaseModel import BaseMeta, BaseModelExtended, BaseSchema
+from marshmallow import fields
+from sqlalchemy import Column, UniqueConstraint, text
+from sqlalchemy.dialects import mysql
+
+class UserModel(BaseModelExtended):
+    __tablename__ = 'users'
+
+    user_name = Column(
+        mysql.VARCHAR(255),
+        nullable=False
+    )
+
+    password = Column(
+        mysql.VARCHAR(255),
+        nullable=False
+    )
+
+    first_name = Column(
+        mysql.VARCHAR(255),
+        nullable=False
+    )
+
+    last_name = Column(
+        mysql.VARCHAR(255),
+        nullable=False
+    )
+
+    phone = Column(
+        mysql.VARCHAR(255),
+        nullable=False
+    )
+    
+    def __repr__(self):
+        return (
+            "UserModel(\
+                id='{id}', \
+                user_name='{user_name}, \
+                password='{password}, \
+                first_name='{first_name}, \
+                last_name='{last_name}, \
+                phone='{phone} \
+                ')"
+        ).format(
+            id=self.id,
+            user_name=self.user_name,
+            password=self.password,
+            first_name=self.first_name,
+            last_name=self.last_name,
+            phone=self.phone
+        )
+
+class UserModelSchema(BaseSchema):
+    id = fields.Integer(dump_only=True)
+
+    class Meta(BaseMeta):
+        model = UserModelSchema
