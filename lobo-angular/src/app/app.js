131ee9de1fc6c7ed620cd245053e78c489d9bcb3
charlesbeckwith <charlie@focalcast.io>
2018-10-13 16:33:33 -0500

    Resolved merge conflicts

--- lobo-angular/src/app/app.js
+++ lobo-angular/src/app/app.js
@@ -5,27 +5,28 @@ import RestConfig from './config/rest.js';
 import '../style/app.css';
 
 let app = () => {
-  return {
+    return {
 
-      // template: require('./app.html'),
-      template: require('./templates/Guides/guides.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
+        // template: require('./app.html'),
+        template: require('./templates/Main/main.html'),
+        controller: 'AppCtrl',
+        controllerAs: 'app'
+    };
 };
 
 class AppCtrl {
-  constructor(Api) {
-    this.url = 'https://github.com/preboot/angular-webpack';
-    this.api = Api;
-  }
+    constructor (Api) {
+        this.url = 'https://github.com/preboot/angular-webpack';
+        this.api = Api;
+    }
 }
-class ApiService{
-    constructor(Restangular, $log){
+class ApiService {
+    constructor (Restangular, $log) {
+
         this.$log = $log;
         this.Restangular = Restangular;
     }
-    users(uuid){
+    users (uuid) {
         this.$log.debug('Hey!');
         return this.Restangular.one('users', uuid).get();
     }
@@ -38,7 +39,5 @@ angular.module(MODULE_NAME, [restangular]).directive('app', app).controller('App
 angular.module(MODULE_NAME).service('Api', ApiService);
 
 angular.module(MODULE_NAME).config(RestConfig);
-
-
 
 export default MODULE_NAME;
---
+++ lobo-angular/src/app/templates/Main/main.js
@@ -0,0 +1,18 @@
+export class HomeComponent {
+    constructor () {
+    }
+    $onInit () {
+
+    }
+    $onDestroy () {
+
+    }
+    static create () {
+        return {
+            bindings: {
+            },
+            controller: HomeComponent,
+            template: require('./main.html')
+        };
+    }
+}
