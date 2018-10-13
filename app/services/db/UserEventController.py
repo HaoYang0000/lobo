fd995c26cd6accf171a09c5970a1e89d1c77c239
Hao Yang <hao.yang@Hao-Yangs-MacBook-Pro.local>
2018-10-13 17:26:29 -0500

    Finish build UserEvent get models

---
+++ app/services/db/UserEventController.py
@@ -0,0 +1,5 @@
+from app.models.UserEventRelationModel import UserEventRelationModel
+from app.services.db.BaseController import BaseService
+
+class UserEventService(BaseService):
+    model = UserEventRelationModel
