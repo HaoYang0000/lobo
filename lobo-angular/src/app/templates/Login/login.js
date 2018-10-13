7dde162a9238996a30866938ac6716795f96bb03
charlesbeckwith <charlie@focalcast.io>
2018-10-13 17:38:38 -0500

    Added login component

---
+++ lobo-angular/src/app/templates/Login/login.js
@@ -0,0 +1,35 @@
+const target = {};
+const loginProxy = new Proxy(target, {
+    get: (target, name) => {
+        return target[name];
+    },
+    set: (target, name, value) => {
+        target[name] = value;
+    }
+});
+export class LoginComponent {
+    constructor (UserApi) {
+        this.api = UserApi;
+        this.loginForm = new Proxy(target, {
+            get: (t,n)=>{
+                return;
+            },
+            set: (t,n,v)=>{
+                t[n]=v;
+            }
+        });
+    }
+    login () {
+        this.api.login(loginProxy);
+    }
+    register(){
+        // If we want to add a register page
+    }
+    static create () {
+        return {
+            bindings: {},
+            controller: LoginComponent,
+            template: require('./login.js')
+        };
+    }
+}
--- lobo-angular/src/app/templates/Main/main.js
+++ lobo-angular/src/app/templates/Main/main.js
@@ -1,5 +1,9 @@
+
 export class HomeComponent {
-    constructor () {
+    constructor (UserApi) {
+
+
+
     }
     $onInit () {
 
